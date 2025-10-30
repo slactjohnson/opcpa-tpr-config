2dd7918a05a1cb46f1e640bc01992f6905a3f62c
slactjohnson <tjohnson@slac.stanford.edu>
2025-10-30 11:03:46 -0700

    Add function to generate 70kHz base pattern with updated diagnostic rates

--- opcpa_tpr_config/xpm_prog.py
+++ opcpa_tpr_config/xpm_prog.py
@@ -159,6 +159,53 @@ def make_base_sequence(offset=None):
     return instrset
 
 
+def make_70k_base_sequence(offset=None):
+    """
+    Setup standard sequence of full rate, 70000, 112, and 7 Hz codes.
+    """
+    # Do some setup
+    instrset = []
+
+    # Insert bucket offset if it is present
+    if offset is not None and offset != 0:
+        instrset.append(FixedRateSync(marker="910kH", occ=offset))
+
+    b0 = len(instrset)
+    instrset.append(ControlRequest([0, 1, 2, 3]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    b1 = len(instrset)
+    instrset.append(ControlRequest([0]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    instrset.append(Branch.conditional(line=b1, counter=0, value=11))
+    b2 = len(instrset)
+    instrset.append(ControlRequest([0, 1]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    b3 = len(instrset)
+    instrset.append(ControlRequest([0]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    instrset.append(Branch.conditional(line=b3, counter=0, value=11))
+    instrset.append(Branch.conditional(line=b2, counter=1, value=623))
+    b4 = len(instrset)
+    instrset.append(ControlRequest([0, 1, 2]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    b5 = len(instrset)
+    instrset.append(ControlRequest([0]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    instrset.append(Branch.conditional(line=b5, counter=0, value=11))
+    b6 = len(instrset)
+    instrset.append(ControlRequest([0, 1]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    b7 = len(instrset)
+    instrset.append(ControlRequest([0]))
+    instrset.append(FixedRateSync(marker="910kH", occ=1))
+    instrset.append(Branch.conditional(line=b7, counter=0, value=11))
+    instrset.append(Branch.conditional(line=b6, counter=1, value=623))
+    instrset.append(Branch.conditional(line=b4, counter=2, value=14))
+    instrset.append(Branch.unconditional(line=b0))
+
+    return instrset
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
